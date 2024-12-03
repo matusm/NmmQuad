cc7bfc9f3ae25a1e359ae328a00f34a02b2745b5
Michael Matus <michael@matus.at>
2024-12-03 08:15:25 +0100

    Draw grid lines

--- Plotter.cs
+++ Plotter.cs
@@ -8,17 +8,14 @@ namespace NmmQuad
     public class Plotter
     {
         private const int imageSize = 1024;
-        private readonly Bitmap bitmap = new Bitmap(imageSize, imageSize, PixelFormat.Format24bppRgb);
-        Pen circlePen = new Pen(Color.BlueViolet, 2F);
-        Pen otherPen = new Pen(Color.Blue, 2F);
+        private Color backgnd = Color.White;
+        private Color dataDot = Color.Red;
+        private Pen circlePen = new Pen(Color.BlueViolet, 1F);
+        private Pen otherPen = new Pen(Color.Blue, 1F);
 
         public Plotter(Quad[] data)
         {
-            using (Graphics g = Graphics.FromImage(bitmap)) 
-            { 
-                g.Clear(Color.White);
-                g.DrawEllipse(circlePen, 10, 10, imageSize - 20, imageSize - 20);
-            }
+            DrawGuideLines();
             PlotLissajous(data);
         }
 
@@ -28,14 +25,29 @@ namespace NmmQuad
             bitmap.Save(filename, f);
         }
 
+        private void DrawGuideLines()
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(backgnd);
+                g.DrawLine(otherPen, 0, 0, imageSize, imageSize);
+                g.DrawLine(otherPen, 0, imageSize, imageSize,0);
+                g.DrawLine(otherPen, 0, imageSize/2, imageSize, imageSize/2);
+                g.DrawLine(otherPen, imageSize / 2, 0, imageSize/2, imageSize);
+                g.DrawEllipse(circlePen, 10, 10, imageSize - 20, imageSize - 20);
+            }
+        }
+
         private void PlotLissajous(Quad[] data)
         {
             foreach (var q in data)
             {
                 int x = (int)q.Sin + imageSize / 2;
                 int y = (int)q.Cos + imageSize / 2;
-                bitmap.SetPixel(x, y, Color.Red);
+                bitmap.SetPixel(x, y, dataDot);
             }
         }
+
+        private readonly Bitmap bitmap = new Bitmap(imageSize, imageSize, PixelFormat.Format24bppRgb);
     }
 }
