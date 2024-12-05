d97a8e08955daffbf15b0ada129a278f498ca6e5
Michael Matus <michael@matus.at>
2024-12-05 17:18:13 +0100

    Fix plot grid errors

--- Plotter.cs
+++ Plotter.cs
@@ -8,17 +8,26 @@ namespace NmmQuad
     public class Plotter
     {
         private const int imageSize = 800;
-        private const double over = 1.1; // 10 % larger
-        private Color backgnd = Color.White;
-        private Color dataDot = Color.Red;
-        private Pen circlePen = new Pen(Color.Blue, 1F);
-        private Pen otherPen = new Pen(Color.LightGray, 1F);
+        private const double over = 1.15; // 15 % larger
+        private static Color backgndCol = Color.White;
+        private static Color dataDotCol = Color.Red;
+        private static Color circleCol = Color.Blue;
+        private static Color axisCol = Color.LightGray;
+        private static Pen circlePen = new Pen(circleCol, 1F);
+        private static Pen axesPen = new Pen(axisCol, 1F);
 
-        public Plotter(Quad[] data)
+        public Plotter(Quad[] normData)
         {
-            ClearBackgnd(backgnd);
-            PlotLissajous(data);
-            DrawGuideLines();
+            ClearBackgnd(backgndCol);
+            PlotLissajous(normData, dataDotCol);
+            DrawAxes(axesPen);
+            DrawCircles(circlePen);
+        }
+
+        public void SaveImage(string filename)
+        {
+            ImageFormat f = new ImageFormat(Guid.NewGuid());
+            bitmap.Save(filename, f);
         }
 
         private void ClearBackgnd(Color backgnd)
@@ -29,40 +38,56 @@ namespace NmmQuad
             }
         }
 
-        public void SaveImage(string filename)
-        {
-            ImageFormat f = new ImageFormat(Guid.NewGuid());
-            bitmap.Save(filename, f);
-        }
-
-        private void DrawGuideLines()
+        private void DrawAxes(Pen pen)
         {
             using (Graphics g = Graphics.FromImage(bitmap))
             {
-                g.DrawLine(otherPen, Transform(-1 * over / Math.Sqrt(2)), Transform(-1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)));
-                g.DrawLine(otherPen, Transform(-1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)), Transform(-1 * over / Math.Sqrt(2)));
-                g.DrawLine(otherPen, Transform(-1 * over), Transform(0), Transform(1 * over), Transform(0));
-                g.DrawLine(otherPen, Transform(0), Transform(-1 * over), Transform(0), Transform(1 * over));
-                g.DrawEllipse(circlePen, Transform(-1), Transform(-1), Transform(1 / over), Transform(1 / over));
-                g.DrawEllipse(circlePen, Transform(-1.05), Transform(-1.05), Transform(1.1 / over), Transform(1.1 / over));
-
+                g.DrawLine(pen, Transform(-1 * over / Math.Sqrt(2)), Transform(-1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)));
+                g.DrawLine(pen, Transform(-1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)), Transform(1 * over / Math.Sqrt(2)), Transform(-1 * over / Math.Sqrt(2)));
+                g.DrawLine(pen, Transform(-1 * over), Transform(0), Transform(1 * over), Transform(0));
+                g.DrawLine(pen, Transform(0), Transform(-1 * over), Transform(0), Transform(1 * over));
             }
         }
 
-        private void PlotLissajous(Quad[] data)
+        private void DrawCircles(Pen pen)
         {
-            foreach (var q in data)
+            double[] radii = new double[] { 0.9, 0.95, 1, 1.05, 1.1};
+            using (Graphics g = Graphics.FromImage(bitmap))
             {
-                int x = Transform(q.Sin);
-                int y = Transform(q.Cos);
-                if (x >= 0 && x < imageSize && y >= 0 && y < imageSize)
-                    bitmap.SetPixel(x, y, dataDot);
+                foreach (double radius in radii)
+                {
+                    g.DrawEllipse(pen, Corner(radius), Corner(radius), Width(radius), Width(radius));
+                }
             }
         }
 
+        private void PlotLissajous(Quad[] data, Color color)
+        {
+            SolidBrush aBrush = new SolidBrush(color);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                foreach (var q in data)
+                {
+                    int x = Transform(q.Sin);
+                    int y = Transform(q.Cos);
+                    g.FillRectangle(aBrush, x, y, 1, 1);
+                }
+            }
+        }
 
         private int Transform(double v) => (int)(imageSize * 0.5 * (v / over + 1));
 
+        private int Corner(double radius)
+        {
+            return Transform(-radius);
+        }
+
+        private int Width(double radius)
+        {
+            return Transform(radius) - Transform(-radius);
+        }
+
         private readonly Bitmap bitmap = new Bitmap(imageSize, imageSize, PixelFormat.Format24bppRgb);
     }
+
 }
