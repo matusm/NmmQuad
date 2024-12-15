8f161d90fddcb8f815d7761aeebfdc8fbab87ca8
Michael Matus <michael@matus.at>
2024-12-15 12:14:53 +0100

    Add two classes for correction
    Heydemann and Matus/Dai correction are ecapsulated in two new classes.

---
+++ Heydemann.cs
@@ -0,0 +1,16 @@
+﻿using Bev.IO.NmmReader.scan_mode;
+
+namespace NmmQuad
+{
+    public class Heydemann
+    {
+        public Quad[] CorrectedData => heydemann.CorrectedQuadratureValues;
+
+        public Heydemann(Quad[] rawData)
+        {
+            heydemann = new NLcorrectionHeydemann(rawData);
+        }
+
+        private readonly NLcorrectionHeydemann heydemann;
+    }
+}
---
+++ MatusDai.cs
@@ -0,0 +1,22 @@
+﻿using Bev.IO.NmmReader.scan_mode;
+
+namespace NmmQuad
+{
+    public class MatusDai
+    {
+        public Quad[] CorrectedData => matusDai.CorrectedQuadratureValues;
+        public double RelativeSquashing => matusDai.RelativeSquashing;
+
+        public MatusDai(Quad[] rawData)
+        {
+            double[] dummyData = new double[rawData.Length];
+            for (int i = 0; i < dummyData.Length; i++)
+            {
+                dummyData[i] = 0;
+            }
+            matusDai = new NLcorrectionDai(dummyData, rawData);
+        }
+
+        private readonly NLcorrectionDai matusDai;
+    }
+}
